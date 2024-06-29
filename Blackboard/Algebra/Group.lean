1804d74aed15e6e54b360062b4cdeb7d8d86f93c
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-06-29 17:48:04 +0800

    (ab)^2
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/Group.lean
+++ Blackboard/Algebra/Group.lean
@@ -11,3 +11,22 @@ variable
 -- then we can see 1g⁻ⁿ = g⁻ⁿ = 1
 theorem inv_order_eq_order (g : G) : orderOf g⁻¹ = orderOf g
   := by simp
+
+theorem aabb
+  (a b : G)
+  : (a * b) ^ 2 = a ^ 2 * b ^ 2 ↔ a * b = b * a
+  := by
+  refine Iff.intro ?a ?b
+  simp [sq]
+  case a =>
+    intros ab
+    rw [mul_assoc, mul_assoc, ←mul_assoc b a b, ←mul_assoc a b b] at ab
+    rw [mul_left_cancel_iff] at ab
+    rw [mul_right_cancel_iff] at ab
+    exact id (Eq.symm ab)
+  case b =>
+    simp [sq]
+    intros comm
+    rw [←mul_assoc, ←mul_assoc, mul_assoc a b a]
+    rw [←comm]
+    simp [mul_assoc]
