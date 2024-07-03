58b4df44e361a4c234d258bd9e873ec73778936b
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-07-04 00:05:44 +0800

    if use `AddMonoid.nsmul`
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/Ring.lean
+++ Blackboard/Algebra/Ring.lean
@@ -8,8 +8,24 @@ theorem distribute_on_2 {R : Type u} [Ring R]
   simp [two_mul]
   rw [Distrib.right_distrib]
 
-theorem general_distribute {R : Type u} [Ring R]
-  (m : Int)
+theorem nat_distribute {R : Type u} [Ring R]
+  (m : ℕ)
+  (a b : R)
+  : AddMonoid.nsmul m (a * b) = (AddMonoid.nsmul m a) * b
+  := by
+  induction m with
+  | zero =>
+    rw [AddMonoid.nsmul_zero (a * b)]
+    rw [AddMonoid.nsmul_zero a]
+    simp
+  | succ n P =>
+    rw [AddMonoid.nsmul_succ n (a * b)]
+    rw [P]
+    rw [←Distrib.right_distrib]
+    rw [Eq.symm (AddMonoid.nsmul_succ n a)]
+
+theorem int_distribute {R : Type u} [Ring R]
+  (m : ℕ)
   (a b : R)
   : m * (a * b) = (m * a) * b
   := by
