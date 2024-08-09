ae011e00c8a7e4727497adc086f4371627b7c7fd
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-09 15:21:19 +0800

    merge condition
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/kernel_and_ideal.lean
+++ Blackboard/Algebra/kernel_and_ideal.lean
@@ -7,10 +7,13 @@ variable
   {R S : Type u}
   [Ring R] [Ring S]
 
+abbrev Ker (ϕ : RingHom R S) := { r : R // ϕ r = 0 }
+
 theorem kernel_is_ideal
   (ϕ : RingHom R S)
-  (a b : { r : R // ϕ r = 0 })
-  : ϕ (a - b) = 0
+  (a b : Ker ϕ)
+  (r : R)
+  : ϕ (a - b) = 0 ∧ ϕ (a * r) = 0 ∧ ϕ (r * a) = 0
   := by
   have ϕa_is_unit := a.prop
   have ϕb_is_unit := b.prop
@@ -19,4 +22,15 @@ theorem kernel_is_ideal
   have lemma₂ : (ϕ a) - (ϕ b) = 0 := by
     rw [ϕa_is_unit, ϕb_is_unit]
     exact zero_sub_zero
-  rw [lemma₁, lemma₂]
+  have lemma₃ : ϕ (a * r) = (ϕ a) * (ϕ r) := by
+    exact RingHom.map_mul ϕ (↑a) r
+  have lemma₄ : ϕ (r * a) = (ϕ r) * (ϕ a) := by
+    exact RingHom.map_mul ϕ r (↑a)
+  have lemma₅ : (ϕ a) * (ϕ r) = 0 := by
+    rw [ϕa_is_unit]
+    exact zero_mul (ϕ r)
+  have lemma₆ : ϕ r * ϕ a = 0 := by
+    rw [ϕa_is_unit]
+    exact mul_zero (ϕ r)
+  rw [lemma₁, lemma₂, lemma₃, lemma₄, lemma₅, lemma₆]
+  exact ⟨rfl, rfl, rfl⟩
