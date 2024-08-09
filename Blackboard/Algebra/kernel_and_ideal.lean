b01965924a5dade98f394b72cd2eebed329a07b6
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-08-09 15:10:21 +0800

    rename
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/kernel_and_ideal.lean
+++ Blackboard/Algebra/kernel_and_ideal.lean
@@ -14,12 +14,9 @@ theorem kernel_is_ideal
   := by
   have ϕa_is_unit := a.prop
   have ϕb_is_unit := b.prop
-  have challege : ϕ (a - b) = (ϕ a) - (ϕ b) := by
+  have lemma₁ : ϕ (a - b) = (ϕ a) - (ϕ b) := by
     exact RingHom.map_sub ϕ ↑a ↑b
-  have challege2 : (ϕ a) - (ϕ b) = 0 := by
+  have lemma₂ : (ϕ a) - (ϕ b) = 0 := by
     rw [ϕa_is_unit, ϕb_is_unit]
     exact zero_sub_zero
-  exact
-    (AddSemiconjBy.eq_zero_iff (ϕ ↑a - ϕ ↑b)
-          (congrFun (congrArg HAdd.hAdd (id (Eq.symm challege))) (ϕ ↑a - ϕ ↑b))).mp
-      challege2
+  rw [lemma₁, lemma₂]
