f4c7e008569a486c2bb3c1e412e07f59d914592d
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-11-15 14:48:38 +0800

    zero morphism
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Category/ZeroObject.lean
+++ Blackboard/Category/ZeroObject.lean
@@ -1,4 +1,6 @@
+import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.Limits.Shapes.ZeroObjects
+import Mathlib.CategoryTheory.Limits.Shapes.ZeroMorphisms
 
 variable
   {C : Type u} [CategoryTheory.Category.{v, u} C]
@@ -30,3 +32,12 @@ theorem construct_zero_morphism
   : Nonempty (A ⟶ B)
   := by
   exact .intro (f ≫ g)
+
+theorem mono_after_g_is_0_then_g_is_0 (H : HasZeroMorphisms C) {X Y Z : C}
+  (g : X ⟶ Y)
+  (f : Y ⟶ Z) [f_is_Mono : Mono f]
+  (P : g ≫ f = 0)
+  : g = 0
+  := by
+  rw [←(H.zero_comp X f)] at P
+  exact (f_is_Mono.right_cancellation g 0) P
