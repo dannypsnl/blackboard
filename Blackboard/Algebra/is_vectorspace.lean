264bf51894b4fc6b1c81d008749cf33305ff74aa
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-12-12 04:13:40 +0800

    most proved
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/is_vectorspace.lean
+++ Blackboard/Algebra/is_vectorspace.lean
@@ -2,8 +2,8 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Analysis.SpecialFunctions.Pow.Real
 
 class VectorSpace
-  (S : Type u) [Add S]
-  (V : Type u) [One V] [Add V]
+  (S : Type u) [One S] [Add S] [Mul S]
+  (V : Type u) [One V] [Add V] [Neg V]
   : Type u where
   sym : {x y : V} → x + y = y + x
   assoc : {x y z : V}
@@ -11,12 +11,13 @@ class VectorSpace
       = x + (y + z)
   unity : {x : V} → 1 + x = x
 
-  inv : V → V
-  cancel : {x : V} → (inv x) + x = 1
+  cancel : {x : V} → (- x) + x = 1
 
   mul : S → V → V
   ax1 : {s1 s2 : S} → {v : V} → mul (s1 + s2) v = mul s1 v + mul s2 v
   ax2 : {c : S} → {x y : V} → mul c (x + y) = mul c x + mul c y
+  -- ax3 : {c d : S} → {v : V} → mul c (mul d v) = mul (c * d) v
+  ax4 : {v : V} → mul 1 v = v
 
 def VReal := { r : Real // 0 < r } deriving
   One, CommMonoid
@@ -27,6 +28,8 @@ instance : Add ℝ>0 where
   add x y := ⟨ x.val * y.val , by refine mul_pos x.property y.property ⟩
 noncomputable instance : Div ℝ>0 where
   div x y := ⟨ x.val / y.val , by refine div_pos x.property y.property ⟩
+noncomputable instance : Neg ℝ>0 where
+  neg x := 1 / x
 noncomputable instance : HPow ℝ>0 ℝ ℝ>0 where
   hPow x y :=
     ⟨ Real.rpow x.val y , Real.rpow_pos_of_pos x.property y ⟩
@@ -36,7 +39,6 @@ theorem outside_cancel {x : ℝ>0} : (1 / x.val) * x.val = 1 := by
   exact one_div_mul_cancel x_ne_zero
 
 noncomputable instance : VectorSpace ℝ ℝ>0 where
-  inv x := 1 / x
   sym {x y} := CommMonoid.mul_comm x y
   assoc {x y z} := mul_assoc x y z
   unity {x} := one_mul x
@@ -51,4 +53,10 @@ noncomputable instance : VectorSpace ℝ ℝ>0 where
   ax2 {c} {x y} := by
     have K :=
       Real.mul_rpow (le_of_lt x.property) (le_of_lt y.property) (z := c)
+    reduce; congr
+  -- ax3 {c d} {v} := by
+  --   have K := Real.rpow_mul (le_of_lt v.property) c d
+  --   reduce; rw [←K]
+  ax4 {v} := by
+    have K := Real.rpow_one v.val
     reduce; congr
---
+++ Blackboard/Basic.lean
@@ -0,0 +1,1 @@
+import Blackboard.Algebra.is_vectorspace
