b75240763a2a3c8fca21f5c01f6e3c9c1766f643
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-04-29 16:59:06 +0800

    not quite good, the proof is not quite explain things

--- Blackboard/Category/Preadditive.lean
+++ Blackboard/Category/Preadditive.lean
@@ -1,4 +1,5 @@
 import Mathlib.CategoryTheory.Preadditive.Basic
+import Mathlib.CategoryTheory.Preadditive.Biproducts
 import Mathlib.CategoryTheory.Limits.Shapes.BinaryBiproducts
 import Mathlib.CategoryTheory.Limits.Shapes.ZeroMorphisms
 
@@ -11,22 +12,12 @@ variable
   {C : Type u}
   [Category.{v, u} C]
   [Preadditive C]
-  [HasZeroObject C]
 
 theorem always_biproduct
   (A B : C)
   [HasBinaryProduct A B]
   : HasBinaryCoproduct A B := by
-  let P := prod A B
-  let coneA : Cone (pair A B) := { pt := A, π := mapPair (𝟙 A) 0 }
-  let coneB : Cone (pair A B) := { pt := B, π := mapPair 0 (𝟙 B) }
-  let sA : A ⟶ P := by
-    exact prod.inl coneA.pt B
-  let sB : B ⟶ P := by
-    exact prod.inr A coneB.pt
-  let PAsCocone : Cocone (pair A B) := { pt := P, ι := mapPair sA sB }
-  have PIsColimit : IsColimit PAsCocone := by
-    sorry
-  let Q : ColimitCocone (pair A B) := { cocone := PAsCocone, isColimit := PIsColimit }
-  have P : Nonempty (ColimitCocone (pair A B)) := Nonempty.intro Q
-  exact { exists_colimit := P  }
+  have isLimit := limit.isLimit (pair A B)
+  have bc := binaryBiconeIsBilimitOfLimitConeOfIsLimit isLimit
+  let cc := BinaryBicone.ofLimitCone isLimit
+  exact HasColimit.mk { cocone := cc.toCocone, isColimit := bc.isColimit }
