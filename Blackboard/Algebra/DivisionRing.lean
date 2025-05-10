de8234cc97e164b2d08fc00604004d597c2b75b5
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-05-10 17:18:14 +0800

    shorten proof
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/DivisionRing.lean
+++ Blackboard/Algebra/DivisionRing.lean
@@ -16,14 +16,12 @@ theorem only_ideals_of_division_ring_are_zero_and_itself
     have K := Ideal.mem_span_singleton_self x
     by_cases H : x = 0
     case pos =>
-      refine Or.inl ?_
-      refine Submodule.ext ?_
       rw [S, H]
-      exact fun x ↦ Eq.to_iff rfl
+      exact Or.inl (Submodule.ext (fun x ↦ Eq.to_iff rfl))
     case neg =>
       have x_isUnit : IsUnit x := Ne.isUnit H
       have one := (Ideal.span_singleton_mul_left_unit x_isUnit) 1
-      refine Or.inr ?_
       simp at one
       rw [S, Ideal.submodule_span_eq]
       rw [one]
+      exact Or.inr rfl
