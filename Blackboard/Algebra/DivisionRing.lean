07dbf796df9cbede6f1ca2afed72f1282a94bd44
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-05-13 00:16:07 +0800

    improve tactic
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/DivisionRing.lean
+++ Blackboard/Algebra/DivisionRing.lean
@@ -11,17 +11,17 @@ theorem only_ideals_of_division_ring_are_zero_and_itself
   : I = Ideal.span { 0 } ∨ I = ⊤ := by
   have P := DivisionRing.isPrincipalIdealRing R
   have I_is_principal := P.principal I
-  exact I_is_principal.principal.elim fun x => by
-    intro S
+  exact I_is_principal.principal.elim fun x S => by
+    rw [S]
     have K := Ideal.mem_span_singleton_self x
     by_cases H : x = 0
     case pos =>
-      rw [S, H]
+      rw [H]
       exact Or.inl (Submodule.ext (fun x ↦ Eq.to_iff rfl))
     case neg =>
       have x_isUnit : IsUnit x := Ne.isUnit H
       have one := (Ideal.span_singleton_mul_left_unit x_isUnit) 1
       simp at one
-      rw [S, Ideal.submodule_span_eq]
+      rw [Ideal.submodule_span_eq]
       rw [one]
       exact Or.inr rfl
