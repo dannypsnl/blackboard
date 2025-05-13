10fc02e99a7d8d5bf2eb0a5938f80e2da34a9992
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-05-13 23:51:09 +0800

    use obtain tactic to replace Exists.elim
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/CommRing.lean
+++ Blackboard/Algebra/CommRing.lean
@@ -10,12 +10,11 @@ variable
 theorem idempotent_is_identity_in_the_ideal_generated_by_it
   [CommRing R]
   (a : R)
-  (H : IsIdempotentElem a)
+  (Idem : IsIdempotentElem a)
   : ∀ i ∈ Ideal.span { a }, a * i = i := by
   intros i hi
   have i_is_elem := Ideal.mem_span_singleton (x := i) (y := a)
   have a_dvd_i := i_is_elem.mp hi
   have ex_C := exists_eq_mul_right_of_dvd a_dvd_i
-  exact ex_C.elim fun _ => by
-    intro h
-    rw [h, ←mul_assoc, H]
+  obtain ⟨c, H⟩ := ex_C
+  rw [H, ←mul_assoc, Idem]
