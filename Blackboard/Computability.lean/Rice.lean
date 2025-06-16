cb0de853ee691c8edfc7bcbe6bd3fdf2baa45af5
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-06-16 15:18:21 +0800

    synthetic rice's theorem

---
+++ Blackboard/Computability.lean/Rice.lean
@@ -0,0 +1,23 @@
+def FixedPointed (A : Type u) : Prop :=
+  ∀ f : A → A, ∃ x : A, f x = x
+
+theorem rice
+  (HasFixedPoint : FixedPointed A)
+  : ∀ f : A → Bool, ∀ x y : A, f x = f y
+  := by
+  intros f x y
+  let g z := if (f z = f y) then x else y
+  have g_fix := HasFixedPoint g
+  obtain ⟨u, H⟩ := g_fix
+  have boolDec : Decidable (f u = f y) := Bool.decEq (f u) (f y)
+  induction boolDec
+  case isTrue fu_eq_fy =>
+    have A : x = g u := (if_pos fu_eq_fy).symm
+    rw [A, H]
+    exact fu_eq_fy
+  case isFalse fu_ne_fy =>
+    have A : g u = y := if_neg fu_ne_fy
+    rw [H] at A
+    have B : f u = f y := congrArg f A
+    have F := fu_ne_fy B
+    exact False.elim (fu_ne_fy B)
