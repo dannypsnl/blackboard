fe5b980bb8ca39f3de34ce997917b3821de68b99
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-06-18 03:00:22 +0800

    cat: inverse of morphism is unique

--- Blackboard/Category/Basic.lean
+++ Blackboard/Category/Basic.lean
@@ -5,3 +5,23 @@ import Mathlib.CategoryTheory.Functor.FullyFaithful
 variable [CategoryTheory.Category.{v, u} C]
 
 open CategoryTheory
+open Category
+
+structure IsInverse {X Y : C} (f : X ⟶ Y) (g : Y ⟶ X) where
+  left : f ≫ g = 𝟙 X
+  right : g ≫ f = 𝟙 Y
+
+theorem inverse_is_unique
+  (Y : C)
+  (f : X ⟶ Y)
+  (g k : Y ⟶ X)
+  (Hg : IsInverse f g)
+  (Hk : IsInverse f k)
+  : g = k := by
+  have A := Hg.right =≫ k
+  have B := Hk.right =≫ k
+  rw [←B] at A
+  simp [assoc] at A
+  rw [Hk.left] at A
+  simp [comp_id] at A
+  exact A
