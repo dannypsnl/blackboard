c52f7c491679aefbd96270cecec111b1773b752d
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-06-18 18:35:55 +0800

    functor preserves Iso
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Category/Basic.lean
+++ Blackboard/Category/Basic.lean
@@ -1,8 +1,11 @@
 import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.CategoryTheory.Iso
 import Mathlib.CategoryTheory.Yoneda
 import Mathlib.CategoryTheory.Functor.FullyFaithful
 
-variable [CategoryTheory.Category.{v, u} C]
+variable
+  [CategoryTheory.Category.{v, u} C]
+  [CategoryTheory.Category.{v, u} D]
 
 open CategoryTheory
 open Category
@@ -25,3 +28,23 @@ theorem inverse_is_unique
   rw [Hk.left] at A
   simp [comp_id] at A
   exact A
+
+-- This is a construction proof
+-- `X ≅ Y` is `Iso X Y`
+def functor_preserves_isomorphism
+  (F : C ⥤ D)
+  (X Y : C)
+  (iso : X ≅ Y)
+  : F.obj X ≅ F.obj Y := by
+  exact {
+    hom := F.map iso.hom
+    inv := F.map iso.inv
+    hom_inv_id := by
+      have fact := iso.hom_inv_id
+      rw [←F.map_comp, ←F.map_id]
+      exact congrArg F.map fact
+    inv_hom_id := by
+      have fact := iso.inv_hom_id
+      rw [←F.map_comp, ←F.map_id]
+      exact congrArg F.map fact
+  }
