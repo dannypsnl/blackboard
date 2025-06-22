3e163fc00beb4459c96771fbf8bce927fa39d5db
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-06-23 02:25:54 +0800

    right adjoint preserves terminal

--- Blackboard/Category/Adjunction.lean
+++ Blackboard/Category/Adjunction.lean
@@ -8,7 +8,7 @@ variable
 open CategoryTheory
 open Category
 
-def left_adjoint_preserves_initial
+def left_adjoint_preserves_initial'
   (I : C)
   (isI : Limits.IsInitial I)
   (F : C ⥤ D)
@@ -24,14 +24,41 @@ def left_adjoint_preserves_initial
       refine (Adjunction.unit_comp_map_eq_iff adj g (isI.to (G.obj X))).mp ?_
       exact Eq.symm (Limits.IsInitial.hom_ext isI (isI.to (G.obj X)) (adj.unit.app I ≫ G.map g))
   }
-
-def left_adjoint_preserves_initial'
+def left_adjoint_preserves_initial
   (I : C)
   (isI : Limits.IsInitial I)
   (F : C ⥤ D)
   (G : D ⥤ C)
   (adj : F ⊣ G)
   : Limits.IsInitial (F.obj I) := by
-  have H : (Y : D) → Unique ((F.obj I) ⟶ Y) := by
-    exact fun Y ↦ left_adjoint_preserves_initial I isI F G adj Y
+  have H : (Y : D) → Unique ((F.obj I) ⟶ Y) :=
+    left_adjoint_preserves_initial' I isI F G adj
   exact Limits.IsInitial.ofUnique (F.obj I)
+
+def right_adjoint_preserves_terminal'
+  (T : D)
+  (isT : Limits.IsTerminal T)
+  (F : C ⥤ D)
+  (G : D ⥤ C)
+  (adj : F ⊣ G)
+  : ∀ X : C, Unique (X ⟶ (G.obj T)) := by
+  intros X
+  let f := G.map (isT.from (F.obj X))
+  exact {
+    default : X ⟶ G.obj T := adj.unit.app X ≫ f
+    uniq := by
+      intros g
+      refine (Adjunction.eq_unit_comp_map_iff adj (isT.from (F.obj X)) g).mpr ?_
+      exact
+        Eq.symm (Limits.IsTerminal.hom_ext isT (isT.from (F.obj X)) (F.map g ≫ adj.counit.app T))
+  }
+def right_adjoint_preserves_terminal
+  (T : D)
+  (isT : Limits.IsTerminal T)
+  (F : C ⥤ D)
+  (G : D ⥤ C)
+  (adj : F ⊣ G)
+  : Limits.IsTerminal (G.obj T) := by
+  have H : (Y : C) → Unique (Y ⟶ (G.obj T)) :=
+    right_adjoint_preserves_terminal' T isT F G adj
+  exact Limits.IsTerminal.ofUnique (G.obj T)
