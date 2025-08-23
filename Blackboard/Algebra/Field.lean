15bac0015cade62012aa8df2c80c1026edbe0129
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-08-23 14:10:07 +0800

    Each field is an integral domain
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

---
+++ Blackboard/Algebra/Field.lean
@@ -0,0 +1,21 @@
+import Mathlib.Algebra.Field.Defs
+import Mathlib.Algebra.GroupWithZero.Defs
+
+theorem Each_field_is_an_integral_domain
+  [Field K]
+  : NoZeroDivisors K := {
+  eq_zero_or_eq_zero_of_mul_eq_zero := by
+    intros a b P
+    refine or_iff_not_imp_left.mpr ?_
+    intro a_ne_zero
+    have H : a * b * (1 / a) = 0 * (1 / a) := by
+      exact congrFun (congrArg HMul.hMul P) (1 / a)
+    have H2 : a * b * (1 / a) = b := by
+      rw [mul_comm, ←mul_assoc, mul_comm _ a]
+      simp
+      have := DivisionRing.mul_inv_cancel a a_ne_zero
+      rw [this]
+      simp
+    rw [←H2, H]
+    simp
+  }
