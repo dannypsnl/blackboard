c5cdb7afe3cc3016d28d392f1cb751a02090fb26
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-12-09 17:08:31 +0800

    linear category
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

---
+++ Blackboard/Category/Linear/LinearCategory.lean
@@ -0,0 +1,11 @@
+import Mathlib.Algebra.Field.Defs
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.CategoryTheory.Linear.Basic
+
+open CategoryTheory
+
+variable
+  [Field K]
+  [Category C]
+  [Preadditive C]
+  [CategoryTheory.Linear K C]
