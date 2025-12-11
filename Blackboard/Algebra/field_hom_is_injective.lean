3058404c3204c064002c19fefaf3ef70c98e9907
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-12-12 03:56:48 +0800

    each field homomorphism is injective

--- Blackboard/Algebra/field_hom_is_injective.lean
+++ Blackboard/Algebra/field_hom_is_injective.lean
@@ -29,3 +29,24 @@ theorem if_fu_is_zero_then_u_must_be_zero
   by_contra P
   have := if_u_is_not_zero_then_fu_cannot_be_zero f is_hom u P
   exact this H
+
+theorem main
+  (f : F → G)
+  (is_hom : IsFieldHom f)
+  : Function.Injective f := by
+  intros x y H
+  have : f x + f (- y) = 0 := by
+    rw [H]
+    rw [←is_hom.preserve_add]
+    rw [←is_hom.preserve_zero]
+    exact congrArg f (add_neg_cancel y)
+  have : f (x - y) = 0 := by
+    rw [←this]
+    rw [←is_hom.preserve_add]
+    exact congrArg f (sub_eq_add_neg x y)
+
+  have : x - y = 0 :=
+    if_fu_is_zero_then_u_must_be_zero f is_hom (x - y) this
+
+  rw [sub_eq_iff_eq_add'.mp this]
+  simp
