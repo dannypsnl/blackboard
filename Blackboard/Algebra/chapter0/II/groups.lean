bd684b0a7148917ae9949ebb3f7050bf6289d13f
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-12-24 17:22:28 +0800

    g |-> inv g is a homomorphism iff G is abelian
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/chapter0/II/groups.lean
+++ Blackboard/Algebra/chapter0/II/groups.lean
@@ -1,6 +1,29 @@
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.Group.Hom.Basic
 
+theorem exercise_4_7 [Group G]
+  : ∀ a b : G, (a * b)⁻¹ = a ⁻¹ * b ⁻¹ ↔ a * b = b * a := by
+  intros a b
+  have IF : (a * b)⁻¹ = a ⁻¹ * b ⁻¹ → a * b = b * a := by
+    intro hom
+    -- `mul_inv_rev` is not hard to understand
+    -- 1. (b * a) * (b * a) ⁻¹ = 1 by definition
+    -- 2. b * a * a⁻¹ * b⁻¹ = 1 by cancel law twice
+    -- in ONLY_IF we also use this
+    have I : (b * a) ⁻¹ = a ⁻¹ * b ⁻¹ := mul_inv_rev b a
+    have II : (a * b) ⁻¹ = (b * a) ⁻¹ := Eq.trans hom (Eq.symm I)
+    have III : (a * b) ⁻¹ ⁻¹ = (b * a) ⁻¹ ⁻¹ := by
+      rw [II]
+    have IV : a * b = (a * b) ⁻¹ ⁻¹ := by rw [inv_inv]
+    have V : b * a = (b * a) ⁻¹ ⁻¹ := by rw [inv_inv]
+    exact Eq.trans IV (Eq.trans III (Eq.symm V))
+  have ONLY_IF : a * b = b * a → (a * b)⁻¹ = a ⁻¹ * b ⁻¹ := by
+    intro comm
+    calc
+      (a * b)⁻¹ = (b * a) ⁻¹ := congrArg Inv.inv comm
+      _ = a ⁻¹ * b ⁻¹ := mul_inv_rev b a
+  exact Iff.intro IF ONLY_IF
+
 -- Exercise 6.15
 -- This is very general due to Sets behaviour, don't even need to assume they are group homomorphisms
 theorem injective_function_is_mono (φ : G → G') (inj : Function.Injective φ)
