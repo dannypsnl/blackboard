acf44c147ee3958b615947d241db4027b29fccca
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-12-24 11:18:58 +0800

    explain
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- Blackboard/Algebra/chapter0/ideals.lean
+++ Blackboard/Algebra/chapter0/ideals.lean
@@ -17,15 +17,28 @@ theorem exercise_4_5
   (I J : Ideal R)
   (H : I + J = ⊤)
   : I * J = I ⊓ J := by
+  -- 這邊因為構成就是 ij
+  -- 1. 根據 j ∈ R 可見 ij ∈ I
+  -- 2. 根據 i ∈ R 可見 ij ∈ J
   have L : I * J ≤ I ⊓ J := by exact Ideal.mul_le_inf
   have R : I ⊓ J ≤ I * J := by
+    -- r ∈ I 而且 r ∈ J
     intro r ⟨r_in_I, r_in_J⟩
+    have : 1 ∈ I + J := (Ideal.eq_top_iff_one (I + J)).mp H
+    -- s ∈ I, t ∈ J
     let ⟨s, s_in_I, t, t_in_J, s_add_t_eq_one⟩ :=
-      Submodule.mem_sup.1 ((Ideal.eq_top_iff_one _).1 H)
+      Submodule.mem_sup.mp this
+    -- r = r * 1
     rw [←mul_one r]
+    -- 1 = s + t 所以 r * 1 = r * (s + t)
     rw [←s_add_t_eq_one]
+    -- r * (s + t) = rs + rt
     rw [mul_add r s t]
+    -- 證明 rs + rt ∈ I * J 需要證明 rs, rt ∈ I * J
     exact Ideal.add_mem (I * J)
+      -- 利用 s ∈ I 跟 r ∈ J 得出相乘 rs ∈ I * J
       (Ideal.mul_mem_mul_rev s_in_I r_in_J)
+      -- 利用 r ∈ I 跟 t ∈ J 得出相乘 rt ∈ I * J
       (Ideal.mul_mem_mul r_in_I t_in_J)
+  -- 用 anti-symmetry 證明等於
   exact le_antisymm L R
