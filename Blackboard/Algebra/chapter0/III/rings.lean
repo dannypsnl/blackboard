3cc59c782069aeba7eb09fbbe4df34884e40d61a
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-12-24 13:04:11 +0800

    fix

--- Blackboard/Algebra/chapter0/III/rings.lean
+++ Blackboard/Algebra/chapter0/III/rings.lean
@@ -1,17 +1,24 @@
 import Mathlib.Algebra.Ring.Basic
 
 abbrev IsZeroDivisor [Ring R] (a : R) : Prop :=
-  ∃ b : R, a * b = 0
+  ∃ b ≠ 0, a * b = 0
 
 theorem zero_divisors_in_comm_ring
   [CommRing R]
   (a1 a2 : R)
-  : IsZeroDivisor a1 ∧ IsZeroDivisor a2 → IsZeroDivisor (a1 * a2) := by
-  intro ⟨Ha1 , Ha2⟩
+  (Ha1 : IsZeroDivisor a1)
+  (Ha2 : IsZeroDivisor a2)
+  (Hb1 : ∀ b : R, Ha1.choose * b = 0 → b = 0)
+  : IsZeroDivisor (a1 * a2) := by
   let b1 := Ha1.choose
   let b2 := Ha2.choose
   exists b1 * b2
-  calc
+  have L : b1 * b2 ≠ 0 := by
+    refine Ne.intro ?_
+    have K : b1 * b2 = 0 → b2 = 0 := Hb1 b2
+    intro N
+    exact Ha2.choose_spec.left (K N)
+  have R := calc
     a1 * a2 * (b1 * b2) = a1 * a2 * b1 * b2 := by
       exact Eq.symm (mul_assoc (a1 * a2) b1 b2)
     _ = a1 * a2 * b1 * b2 := by
@@ -21,5 +28,6 @@ theorem zero_divisors_in_comm_ring
     _ = (a1 * b1) * (a2 * b2) := by
       exact mul_assoc (a1 * b1) a2 b2
     _ = 0 := by
-      rw [Ha1.choose_spec]
+      rw [Ha1.choose_spec.right]
       exact zero_mul (a2 * b2)
+  exact ⟨L, R⟩
