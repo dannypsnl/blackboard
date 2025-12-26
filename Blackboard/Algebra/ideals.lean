3789a36414403315410d8b5b8ab2dcf6cf41225d
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-12-26 21:33:58 +0800

    0.4
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

---
+++ Blackboard/Algebra/ideals.lean
@@ -0,0 +1,14 @@
+import Mathlib.Algebra.Ring.Defs
+import Mathlib.RingTheory.Ideal.Defs
+import Mathlib.RingTheory.Ideal.Span
+import Mathlib.RingTheory.Ideal.Operations
+
+-- https://people.math.osu.edu/snapp.14/IntroCommAlg.pdf
+variable
+  [CommRing R]
+
+theorem exercise_0_4
+  (I J K : Ideal R)
+  : I * (J + K) = I * J + I * K := by
+  rw [add_eq_sup, add_eq_sup]
+  exact Ideal.mul_sup I J K
