71918dca525546f76576382e1cb40fb86f60efd3
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-12-28 12:27:34 +0800

    0.8 (ii)

--- Blackboard/Algebra/Introduction to Commutative Algebra/00-backgrounds.lean
+++ Blackboard/Algebra/Introduction to Commutative Algebra/00-backgrounds.lean
@@ -14,10 +14,31 @@ theorem exercise_0_4
 
 notation "√" I => Ideal.radical I
 
-theorem proposition_0_8_I
-  (I : Ideal R)
+theorem proposition_0_8_I (I : Ideal R)
   : I ≤ √ I := by
   intro i ih
   refine Ideal.mem_radical_iff.mpr ?_
   exists 1
   exact (pow_one i).symm ▸ ih
+
+lemma ideal_order {I J K : Ideal R} : I ≤ J → J ≤ K → I ≤ K :=
+  fun p q ⦃_⦄ i ↦ q (p i)
+
+theorem proposition_0_8_II (I : Ideal R)
+  : (√ I) = √ √ I := by
+  have L : (√ I) ≤ √ √ I := proposition_0_8_I (√ I)
+  have R : (√ √ I) ≤ √ I := by
+    intros i ih
+    -- 1. unroll `i ∈ √√I`
+    have f : ∃ n, i ^ n ∈ √ I := Ideal.mem_radical_iff.mp ih
+    -- 2. unroll `i^n ∈ √I` nested in `∃`
+    have s : ∃ n, (i ^ f.choose) ^ n ∈ I := Ideal.mem_radical_iff.mp f.choose_spec
+    refine Ideal.mem_radical_iff.mpr ?_
+    -- choose `n * m`
+    exists f.choose * s.choose
+    -- rewrite i^(n * m) to (i^n)^m
+    rw [pow_mul]
+    -- and that's exactly the second unroll
+    have : (i ^ f.choose) ^ s.choose ∈ I := s.choose_spec
+    exact this
+  exact le_antisymm L R
